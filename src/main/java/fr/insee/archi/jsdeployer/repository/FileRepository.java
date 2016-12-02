798e42202474dbdd29e1c2c4841b9a5205949a9b
romaintailhurat <romain.tailhurat@gmail.com>
2016-12-02 11:16:11 +0100

    Saving zip entries to disk

--- src/main/java/fr/insee/archi/jsdeployer/repository/FileRepository.java
+++ src/main/java/fr/insee/archi/jsdeployer/repository/FileRepository.java
@@ -1,10 +1,13 @@
 package fr.insee.archi.jsdeployer.repository;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -15,6 +18,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import fr.insee.archi.jsdeployer.Constants;
 import fr.insee.archi.jsdeployer.DeployerController;
 
 @Service
@@ -40,14 +44,15 @@ public class FileRepository implements AppRepository {
 	@Override
 	public boolean storeApp(String appName, byte[] content) {
 		recordContent(appName, content);
-		File f = getFileFromBytes(content);
-		saveContent(f);		
+		saveContent(appName, getFileFromBytes(content));	
 		return true; // TODO handle wrong cases
 	}
 
 	private void recordContent(String appName, byte[] content) {
-		log.info(String.format("Repo from properties : %s", repository));
-		Path path = Paths.get(repository + appName + ".zip");
+		String historyPath = repository + Constants.HISTORY_REPO;
+		log.info("Recording raw content to " + historyPath);
+		makeDirIfNeeded(historyPath);
+		Path path = Paths.get(historyPath + appName + ".zip");
 		try {
 			Files.write(path, content);			
 		} catch (IOException e) {
@@ -56,16 +61,41 @@ public class FileRepository implements AppRepository {
 		}
 	}
 
-	private void saveContent(File f) {
+	private void saveContent(String appName, File f) {
 		ZipFile z;
+		String globalAppsPath = repository + Constants.APPS_REPO ; //File.separator
+		makeDirIfNeeded(globalAppsPath);
+		String appPath = globalAppsPath + appName + File.separator;
+		log.info("Saving content to " + appPath);
+		makeDirIfNeeded(appPath);
 		try {
 			z = new ZipFile(f);
-			for (Enumeration<? extends ZipEntry> entries = z.entries(); entries.hasMoreElements();)
-				log.info(entries.nextElement());
+			for (Enumeration<? extends ZipEntry> entries = z.entries(); entries.hasMoreElements();) {
+				ZipEntry ze = entries.nextElement();
+				log.info("Saving zip entries " + ze.getName());
+				Path targetFilePath = Paths.get(appPath + ze.getName());
+				if (ze.isDirectory()) {
+					log.info("Zip entry is a directory");
+					makeDirIfNeeded(targetFilePath.toString());
+				} else {
+					log.info("Zip entry is a file");
+					//Files.createFile(targetFilePath);
+					InputStream is = z.getInputStream(ze);
+					Files.copy(is, targetFilePath);
+				}				
+			}				
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private void makeDirIfNeeded(String appPath) {
+		log.info("Creating dir " + appPath);
+		File destDir = new File(appPath);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
 	}
 
 	private File getFileFromBytes(byte[] content) {
